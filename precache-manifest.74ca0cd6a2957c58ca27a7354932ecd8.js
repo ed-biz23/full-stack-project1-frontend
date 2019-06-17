3dc2d627fe5d3f4507b4b6665d5ded4440037a4f
Edward Biswas <edbiswas@gmail.com>
2019-06-17 13:53:55 -0400

    Updates

---
+++ precache-manifest.74ca0cd6a2957c58ca27a7354932ecd8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80cf5a3cadcea1e25583676611f8b744",
+    "url": "/full-stack-project1-frontend/index.html"
+  },
+  {
+    "revision": "23923be695c360448a66",
+    "url": "/full-stack-project1-frontend/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "ad319f621cfc6385eaaa",
+    "url": "/full-stack-project1-frontend/static/css/main.2355f87d.chunk.css"
+  },
+  {
+    "revision": "23923be695c360448a66",
+    "url": "/full-stack-project1-frontend/static/js/2.26b69e89.chunk.js"
+  },
+  {
+    "revision": "ad319f621cfc6385eaaa",
+    "url": "/full-stack-project1-frontend/static/js/main.1837dddd.chunk.js"
+  },
+  {
+    "revision": "059f4ea50883d89d80a2",
+    "url": "/full-stack-project1-frontend/static/js/runtime~main.bd6babf9.js"
+  },
+  {
+    "revision": "f44453ffa21d1be83c09b186caa62a84",
+    "url": "/full-stack-project1-frontend/static/media/books.f44453ff.png"
+  },
+  {
+    "revision": "5aa8ddbbcf70d3a7b1c1a308c699a912",
+    "url": "/full-stack-project1-frontend/static/media/open-books.5aa8ddbb.jpg"
+  }
+]);
